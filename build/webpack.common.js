cd3e9efbbe318feb49601ce33ef2fa3e707e81b8
xuqichuang <xqc1319681561@163.com>
2018-06-28 09:21:31 +0800

    把webpack配置文件放置到build文件夹中，把dist文件打包到项目根目录

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-        app: './src/index.js'
+        app: './src/main.js'
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
@@ -17,7 +17,7 @@ module.exports = {
     ],
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, '../dist')
     },
     module: {
         rules: [{
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import printMe from './print.js';
 import {
     cube
 } from './math.js';
-
+console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV !== 'production') {
     console.log('Looks like we are in development mode!');
 }
